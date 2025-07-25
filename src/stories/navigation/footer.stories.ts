7d75a94097dfc55e48e27946789db546f49d0466
Nick Sacy <nicholas.a.sacy@questdiagnostics.com>
2025-07-25 11:59:25 -0700

    Updated footer

--- src/stories/navigation/footer.stories.ts
+++ src/stories/navigation/footer.stories.ts
@@ -33,11 +33,33 @@ const meta: Meta<QDSFooterComponent> = {
 export default meta;
 
 export const CITFooter = {
-    args: {}
+    args: {},
+    parameters: {
+        docs: {
+            source: {
+                language: 'html',
+                type: 'code',
+                code: `
+Please contact the Design System team for the Footer code snippet.
+                `.trim()
+            }
+        }
+    }
 };
 
 export const HIPAAFooter = {
     args: {
         type: 'hipaa'
+    },
+    parameters: {
+        docs: {
+            source: {
+                language: 'html',
+                type: 'code',
+                code: `
+Please contact the Design System team for the Footer code snippet.
+                `.trim()
+            }
+        }
     }
 };
